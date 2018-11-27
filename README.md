4561eaca165125aa671abb8fadfd9a4234d31a2d
schiette <pieterschiettecatte@gmail.com>
2018-11-27 17:26:11 -0500

    added core for random rotate

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ QuickRotate
   - rotates the selection 90 degrees its origin
   - ideal for placing furniture, assets, vegetation etc
   - tip: add a shortcut through preferences in Formit
-  test
-  
--- RandomRotate/randomrotate.js
+++ RandomRotate/randomrotate.js
@@ -1,23 +1,20 @@
-QuickRotatePlugin = {};
+RandomRotatePlugin = {};
 
 // TODO: figure out why combining these into a single function with a direction argument gets the LCS translation wrong
 
-QuickRotatePlugin.ButtonR = function()
+RandomRotatePlugin.ButtonR = function()
 {
     FormIt.UndoManagement.BeginState();
 
     var selections = FormIt.Selection.GetSelections();
     //console.log("Selections: " + JSON.stringify(selections));
-    var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
+    //var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
     //console.log("AABB: " + JSON.stringify(AABB));
     
     var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
     //console.log("Current history: " + JSON.stringify(nHistoryID));
 
-    var midpoint = WSM.Geom.Point3d((AABB.upper.x + AABB.lower.x) * 0.5, (AABB.lower.y + AABB.upper.y) * 0.5, AABB.lower.z);
-    var zvector = WSM.Geom.Vector3d(0, 0, 1)	
-    var rotationcenter = WSM.Geom.Line3d(midpoint,zvector)	
-    var angle = 90 * Math.PI /180;
+    var zvector = WSM.Geom.Vector3d(0, 0, 1)
     
     // for each object selected, execute the rotate
     for (var j = 0; j < selections.length; j++)
@@ -29,14 +26,23 @@ QuickRotatePlugin.ButtonR = function()
         // get objectID of the current selection
         var nObjectID = selections[j]["ids"][historyDepth]["Object"];
         //console.log("Object ID: " + JSON.stringify(nObjectID));
+
+        //get bounding box of current selection
+        var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections[j], WSM.Utils.CoordSystem.HCS);
+
+        var midpoint = WSM.Geom.Point3d((AABB.upper.x + AABB.lower.x) * 0.5, (AABB.lower.y + AABB.upper.y) * 0.5, AABB.lower.z);
         
+        var rotationcenter = WSM.Geom.Line3d(midpoint, zvector)
+        var randomizedangle = Math.floor(Math.random() * 361);
+        var angle = randomizedangle * Math.PI / 180;
+
         // define the rotation
         var rotation = WSM.Geom.MakeRotationTransform(rotationcenter,angle)	
         // change geometry using the given translation
         WSM.APITransformObjects(nHistoryID,nObjectID,rotation)	
      }
 
-    FormIt.UndoManagement.EndState("Quick Rotate");
-    console.log("Quick Rotated " + selections.length + " object(s) in History " + nHistoryID);
+    FormIt.UndoManagement.EndState("Random Rotate");
+    console.log("Randomly Rotated " + selections.length + " object(s) in History " + nHistoryID);
 }
-FormIt.Commands.RegisterJSCommand("QuickRotatePlugin.ButtonR");
+FormIt.Commands.RegisterJSCommand("RandomRotatePlugin.ButtonR");
